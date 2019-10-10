9f876e4cc93df9518ae4630cf3eb459dea662803
Greg Allen <greg@firstandthird.com>
2019-10-10 13:25:06 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # arc-macro-custom-domain
+
+- only works on staging (for now)
+- can override domain with `ARC_DOMAIN`
+
+## Example .arc
+```
+@app
+example
+
+@http
+get /
+
+@macros
+arc-macro-custom-domain
+
+@customDomain
+{domain}
+{route53 host}
+{certArn}
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = function(arc, cloudformation, stage) {
+  //only on stage
+  if (stage !== 'staging' || !arc.customDomain) {
+    return cloudformation;
+  }
+
+  const domainName = process.env.ARC_DOMAIN || arc.customDomain[0];
+  const route53Host = arc.customDomain[1];
+  const certArn = arc.customDomain[2];
+  const restId = Object.keys(cloudformation.Resources)[0]; //probably could make this better
+
+  cloudformation.Resources.ApiGatewayDomain = {
+    Type: 'AWS::ApiGateway::DomainName',
+    Properties: {
+      RegionalCertificateArn: certArn,
+      DomainName: domainName,
+      EndpointConfiguration: {
+        Types: ['REGIONAL']
+      }
+
+    }
+  };
+  cloudformation.Resources.ApiGatewayMapping = {
+    Type: 'AWS::ApiGateway::BasePathMapping',
+    Properties: {
+      BasePath: '',
+      DomainName: domainName,
+      Stage: stage,
+      RestApiId: {
+        Ref: restId
+      }
+    }
+  };
+  //route53
+  cloudformation.Resources.DomainDns = {
+    Type: 'AWS::Route53::RecordSetGroup',
+    Properties: {
+      HostedZoneName: `${route53Host}.`,
+      Comment: 'Route to api gateway stage',
+      RecordSets: [{
+        Name: domainName,
+        Type: 'A',
+        AliasTarget: {
+          HostedZoneId: { 'Fn::GetAtt': ['ApiGatewayDomain', 'RegionalHostedZoneId'] },
+          DNSName: { 'Fn::GetAtt': ['ApiGatewayDomain', 'RegionalDomainName'] }
+        }
+      }]
+    }
+  };
+  cloudformation.Outputs.Domain = {
+    Description: 'Domain',
+    Value: `https://${domainName}`
+  };
+  return cloudformation;
+};
